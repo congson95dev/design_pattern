87bff30cc413c3ac44e1c66df324405444300087
fudo <saxsax199502a@gmail.com>
2023-05-27 09:24:24 +0700

    update strategy pattern

--- strategy.py
+++ strategy.py
@@ -1,83 +1,51 @@
-# Định nghĩa interface tính toán 
+# Định nghĩa interface tính toán
+from abc import ABC, abstractmethod
 
-from abc import ABC, abstractmethod 
 
-  
+class CalculateStrategy(ABC):
+    @abstractmethod
+    def calculate(self, price: float) -> float:
+        pass
 
-class CalculateStrategy(ABC): 
 
-    @abstractmethod 
+# Các class triển khai CalculateStrategy
+class RetailPriceStrategy(CalculateStrategy):
+    def calculate(self, price: float) -> float:
+        return price * 1.2
 
-    def calculate(self, price: float) -> float: 
 
-        pass 
+class WholesalePriceStrategy(CalculateStrategy):
+    def calculate(self, price: float) -> float:
+        return price * 1.1
 
-  
 
-# Các class triển khai CalculateStrategy 
+class PromotionalPriceStrategy(CalculateStrategy):
+    def calculate(self, price: float) -> float:
+        return price * 0.8
 
-class RetailPriceStrategy(CalculateStrategy): 
 
-    def calculate(self, price: float) -> float: 
+# Class sử dụng Strategy Pattern
+class Product:
+    def __init__(self):
+        self.calculate_strategy = None
 
-        return price * 1.2 
 
-  
+    def set_calculate_strategy(self, strategy: CalculateStrategy):
+        self.calculate_strategy = strategy
 
-class WholesalePriceStrategy(CalculateStrategy): 
 
-    def calculate(self, price: float) -> float: 
+    def calculate_price(self, price: float) -> float:
+        return self.calculate_strategy.calculate(price)
 
-        return price * 1.1 
-
-  
-
-class PromotionalPriceStrategy(CalculateStrategy): 
-
-    def calculate(self, price: float) -> float: 
-
-        return price * 0.8 
-
-  
-
-# Class sử dụng Strategy Pattern 
-
-class Product: 
-
-    def __init__(self): 
-
-        self.calculate_strategy = None 
-
-  
-
-    def set_calculate_strategy(self, strategy: CalculateStrategy): 
-
-        self.calculate_strategy = strategy 
-
-  
-
-    def calculate_price(self, price: float) -> float: 
-
-        return self.calculate_strategy.calculate(price) 
-
-  
 
 # Sử dụng 
+product = Product()
 
-product = Product() 
+product.set_calculate_strategy(RetailPriceStrategy())
+print(product.calculate_price(100)) # Kết quả: 120.0
 
-product.set_calculate_strategy(RetailPriceStrategy()) 
+product.set_calculate_strategy(WholesalePriceStrategy())
+print(product.calculate_price(100))# Kết quả: 110.0
 
-print(product.calculate_price(100)) # Kết quả: 120.0 
-
-  
-
-product.set_calculate_strategy(WholesalePriceStrategy()) 
-
-print(product.calculate_price(100))# Kết quả: 110.0 
-
-  
-
-product.set_calculate_strategy(PromotionalPriceStrategy()) 
-
-print(product.calculate_price(100)) # Kết quả: 80.0 
+product.set_calculate_strategy(PromotionalPriceStrategy())
+print(product.calculate_price(100)) # Kết quả: 80.0
